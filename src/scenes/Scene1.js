0304b05f35d03fbe565740e653c0bf82c16880b3
Adam Mathieson <mathieson1222@gmail.com>
2020-08-20 18:59:30 -0700

    positioning for doorbell cam product and info animations

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -35,34 +35,33 @@ const Scene1 = () => {
             tl.to("#scene-1", 1, {opacity: 1})
 
             // Walk up to porch 
-            tl.fromTo("#guy", 3, {x: -200}, {x: 290, ease: 'none'})
-            tl.to("#guy-side", {display: "none", duration: 0})
-            tl.to("#guy-back", {display: "block", duration: 0})
-            //Zoom to porch
-            tl.to("#scene-1", 3,{attr:{viewBox: "250 250 400 200"}})
-            tl.to("#guy", 3, {scale:0.38 , y: -3, onComplete: stopWalking}, "-=3")
+            // tl.fromTo("#guy", 3, {x: -200}, {x: 290, ease: 'none'})
+            // tl.to("#guy-side", {display: "none", duration: 0})
+            // tl.to("#guy-back", {display: "block", duration: 0})
+            // //Zoom to porch
+            // tl.to("#scene-1", 3,{attr:{viewBox: "250 250 400 200"}})
+            // tl.to("#guy", 3, {scale:0.38 , y: -3, onComplete: stopWalking}, "-=3")
 
             // zoom in product 2
-            tl.to("#scene-1", 2, {attr:{viewBox: "470 310 20 15"}})
+            tl.to("#scene-1", 2, {attr:{viewBox: "464 344.5 15 8"}})
             tl.fromTo("#product-display-2", .32, {display: "block", opacity: 0}, {opacity: 1})
-            tl.to("#product-title-2", 0.5, {display: "block", scale: .8, x: 4})
-            tl.to("#product-info-2", 0.5, {display: "block", scale: .7, x: 4})
-            tl.to("#product-price-2", 0.5, {display: "block", scale: .7, x: 4})
+            tl.fromTo("#product-title-2, #product-info-2", 0.5, {display: "none", y:1, opacity: 0}, {display: 'block', y:0, opacity: 1, stagger: 0.1})
+            tl.fromTo("#product-price-2", 0.5, {display: "none", y: 1.5}, {display: "block", y:0.5}, "-=0.2")
             tl.to("#product-card-2", 0.4, {opacity: 0, delay: 2})
             tl.to("#product-display-2", 0.5, {opacity: 0})
             
             // zoom in on product 1
             tl.to("#scene-1", 1.5, {attr:{viewBox: "460 353 20 15", delay: 1}})
-            tl.fromTo("#product-display", .32, {display: "block", opacity: 0}, {opacity: 1})
-            tl.to("#product-title", 0.5, {display: "block", scale: .8, x: 3})
-            tl.to("#product-info", 0.5, {display: "block", scale: .7, x: 3})
-            tl.to("#product-price", 0.5, {display: "block", scale: .7, x: 3})
-            tl.to("#product-card", 0.4, {opacity: 0, delay:2})
-            tl.to("#product-display", 0.5, {opacity: 0})
+            // tl.fromTo("#product-display", .32, {display: "block", opacity: 0}, {opacity: 1})
+            // tl.to("#product-title", 0.5, {display: "block", scale: .8, x: 3})
+            // tl.to("#product-info", 0.5, {display: "block", scale: .7, x: 3})
+            // tl.to("#product-price", 0.5, {display: "block", scale: .7, x: 3})
+            // tl.to("#product-card", 0.4, {opacity: 0, delay:2})
+            // tl.to("#product-display", 0.5, {opacity: 0})
 
-            // zoom out and fade
-            tl.to("#scene-1", 3, {attr:{viewBox: "250 250 400 200"}})
-            tl.to("#scene-1", 0.4, {opacity: 0, onComplete: next})
+            // // zoom out and fade
+            // tl.to("#scene-1", 3, {attr:{viewBox: "250 250 400 200"}})
+            // tl.to("#scene-1", 0.4, {opacity: 0, onComplete: next})
     }, [next])
 
     return (
